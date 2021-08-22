6dfc0fe2012c761c63cfe8ea1dbf0f9888c81350
Jason Bedney <35877774+Jbedney@users.noreply.github.com>
2021-08-22 15:35:46 -0400

    Update HR Data sample.py

--- HR Data sample.py
+++ HR Data sample.py
@@ -1,96 +1,80 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[89]:
-
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-# In[90]:
-
-
+# read in data
 df = pd.read_csv("core_dataset.csv")
 
-
-# In[91]:
-
-
+# view schema
 df.info()
 
-
-# In[92]:
-
-
+# view fields
 df.head()
 
-
-# In[93]:
-
-
+# filter by active employees
 df = df.where(df['Employment Status'] == 'Active')
 
-
-# In[94]:
-
-
+# remove whitespace in department names
 df['Department'] = df['Department'].str.strip()
 
-
-# In[95]:
-
-
+# capitalize fields
 df['Sex'] = df['Sex'].str.capitalize()
 
-
-# In[96]:
-
-
+# pivot to calculate average pay rate by department
 by_department = pd.pivot_table(df, values='Pay Rate', index=['Department'],
                     columns=['Sex'], aggfunc=np.mean)
 
+# calculate absolute value of pay gap
+by_department['gap'] = (by_department['Male'] - by_department['Female']).abs()
 
-# In[97]:
-
-
-by_department['gap'] = by_department['Male'] - by_department['Female']
-
-
-# In[106]:
-
-
+# sort by pay gap
 by_department = by_department.sort_values(by=['gap'],ascending=False)
 by_department
 
+# plot chart
+female = by_department['Female']
+male = by_department['Male']
 
-# In[105]:
+X = by_department.index
 
-
-by_department['gap'].plot(x='Department',kind='bar')
-
+X_axis = np.arange(len(X))
+  
+plt.bar(X_axis - 0.2, female, 0.4, label = 'Female')
+plt.bar(X_axis + 0.2, male, 0.4, label = 'Male')
+  
 plt.title("Pay Disparity By Department")
 plt.xlabel("Department")
-plt.ylabel("Pay Gap From Male to Female")
+plt.ylabel("Pay Rate")    
+plt.xticks(X_axis, X,rotation=45)
+plt.legend()
+plt.show()
 
-
-# In[101]:
-
-
-by_position = pd.pivot_table(df, values='Pay Rate', index=['Department','Position'],
+# pivot to calculate average pay rate by position
+by_position = pd.pivot_table(df, values='Pay Rate', index=['Position'],
                     columns=['Sex'], aggfunc=np.mean)
-by_position['gap'] = by_position['Male'] - by_position['Female']
+
+# calculate absolute value of pay gap
+by_position['gap'] = (by_position['Male'] - by_position['Female']).abs()
+
+# sort by pay gap
 by_position = by_position.sort_values(by=['gap'],ascending=False)
 by_position
 
+# plot chart
+female = by_position['Female']
+male = by_position['Male']
 
-# In[102]:
+X = by_position.index
 
+X_axis = np.arange(len(X))
 
-by_position['gap'].plot(x='Position',kind='bar')
-
+f = plt.figure()
+f.set_figwidth(15)
+plt.bar(X_axis - 0.2, female, 0.4, label = 'Female')
+plt.bar(X_axis + 0.2, male, 0.4, label = 'Male') 
 plt.title("Pay Disparity By Position")
 plt.xlabel("Position")
-plt.ylabel("Pay Gap From Male to Female")
-
+plt.ylabel("Pay Rate")    
+plt.xticks(X_axis, X,rotation=90)
+plt.legend()
+plt.show()
